91cebc501d2c618f0aaf0d3380fd1a0b1d5302d2
ItzMeCrazy <95604642+ItzMeCrazy@users.noreply.github.com>
2021-12-22 19:22:43 +0530

    Initial commit

---
+++ config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  databaseToken: 'mongodb+srv://Zone:Zone@cluster0.z2bwm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+
+  mainGuild: '915249589743935529',
+  logChannel: '919252829531680799',
+  mailChannel: '915250190393421904',
+  modRoles: ['915250160823578635'],
+
+  status: 'for dms!',
+  color: 0xfcfcfc,
+  prefix: 'm?',
+  msgPrefix: 'Team Ice',
+};
